b319ca55589f1facb29842bcbc34a798d39747f2
satya162 <137689968+satya162@users.noreply.github.com>
2023-07-24 22:01:16 +0530

    Update project1py.py

--- project1py.py
+++ project1py.py
@@ -1,10 +1,19 @@
-from turtle import *
-color('red', 'yellow')
-begin_fill()
-while True:
-    forward(200)
-    left(170)
-    if abs(pos()) < 1:
-        break
-end_fill()
-done()
+import turtle
+
+# Set up the turtle
+screen = turtle.Screen()
+screen.bgcolor("black")
+turtle.speed(0)
+
+# Draw a flower pattern
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+for _ in range(36):
+    for _ in range(6):
+        turtle.color(colors[_ % 7])
+        turtle.forward(100)
+        turtle.left(60)
+    turtle.right(10)
+
+# Exit on click
+turtle.exitonclick()
